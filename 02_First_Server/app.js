c283840ab9e9705f3ac46287fd64ca867046f13b
Mette Munch <meha0005@stud.ek.dk>
2025-09-11 11:41:38 +0200

    efter undervisningsgang 1109

--- 02_First_Server/app.js
+++ 02_First_Server/app.js
@@ -7,6 +7,8 @@ const express = require("express"); //importering
 const app = express(); //instantieringen
 //const app = require("express")();   kan også gøres på denne måde, men 
 
+app.use(express.json()); //sets up body parsing
+
 //console.log(express); //viser alle biblioteks funktioner
 //console.log(app);
 
@@ -46,6 +48,27 @@ app.get("/bag/:item1/:item2", (req,res) => {
     const { item1, item2} = req.params;
     console.log(req.params);
     res.send({result: `Your bag contains: ${item1} and ${item2}`}); //returnerer et JSON objekt
+});
+
+
+// assignment create a GET /urls route, create a query string with the length of "medium" and spiciness level of 6
+// /urls?length=medium&spiciness=6
+
+app.get("/urls", (req, res) => {
+    console.log(req.query)
+    res.send({data: req.query})
+});
+
+//man kan sende data på 3 måder
+ 
+app.post("/subjects", (req, res) => {
+    console.log(req.body);
+    res.send({data: req.body});  //selvom body allerede er json så god ide altid at sende som json med data
+});
+
+console.log(__dirname); //dirname er lig med pwd og kan give absolute path uden at det kun kan bruges lokalt
+app.get("/fashionbrands", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(8080); //hvorfor 8080 - er http developer port som standard
--- ASSIGNMENTS/greek_gods_api/app.js
+++ ASSIGNMENTS/greek_gods_api/app.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 const app = express();
-app.listen(8080); //8080 er http developer port som standard
+
 
 //initial config array with greek gods/goddess
 const greekGods = [
-  { id: 1, name: "Zues", domain: "Sky" },
+  { id: 1, name: "Zeus", domain: "Sky" },
   { id: 2, name: "Athena", domain: "Wisdom" },
   { id: 3, name: "Poseidon", domain: "Sea" },
   { id: 4, name: "Apollo", domain: "Sun" },
@@ -14,16 +14,28 @@ const greekGods = [
 
 //API to get all greek gods
 app.get("/gods", (req, res) => {
-  /* res.send({
-    result: greekGods
-  }); */
-  res.json(greekGods);
+  res.send({
+    data: greekGods
+  }); 
+  //res.json(greekGods);  //brug ikke .json legacy
 });
 
 //API to get an specific greek god
 app.get("/gods/:id", (req, res) => {
   const id = Number(req.params.id); //req.params vil alene altid returnere en String, derfor ændres..
-  const god = greekGods.find((g) => g.id === id);
-  res.send({ result: `With id ${id} you find ${god.name} who is god for the ${god.domain}`});
-  //res.json(god);
+  const god = greekGods.find((greekGod) => greekGod.id === id);
+  
+  //res.json(god);  //brug ikke .json
+
+  if(!god) { //vi vil gerne håndtere fejlen først
+    res.status(404).send({ errorMessage: `Greek god not found with id: ${id}`});
+
+  } else {
+
+    res.send({ data: `With id ${id} you find ${god.name} who is god for the ${god.domain}`});
+  }
 });
+
+
+
+app.listen(8080); //8080 er http developer port som standard  denne skal være i bunden ellers kan der opstå nogle bugs
