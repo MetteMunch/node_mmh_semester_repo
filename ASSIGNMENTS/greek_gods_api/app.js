bee5bbb593328b6ab4decfe6cd13f6534ba62298
Mette Munch <meha0005@stud.ek.dk>
2025-09-17 10:35:14 +0200

    Finished CRUD

--- ASSIGNMENTS/greek_gods_api/app.js
+++ ASSIGNMENTS/greek_gods_api/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 
 //initial config array with greek gods/goddess
@@ -12,12 +13,14 @@ const greekGods = [
   { id: 4, name: "Apollo", domain: "Sun" },
 ];
 
+//------------  Routes ----------------------
+
 //API to get all greek gods
 app.get("/greekgods", (req, res) => {
   res.send({
     data: greekGods
   }); 
-  //res.json(greekGods);  //brug ikke .json legacy
+  //res.json(greekGods);  //brug ikke det er .json legacy
 });
 
 //API to get an specific greek god
@@ -32,6 +35,51 @@ app.get("/greekgods/:id", (req, res) => {
   }
 });
 
+//API to create a new greek god
+app.post("/greekgods", (req, res) => {
+
+if(!req.body) {
+  return res.status(400).send({
+    errorMessage: "Request body can not be empty",
+  })
+}
+
+const newGreekGod = {
+  id: greekGods.length>1?greekGods[greekGods.length-1].id+1 : 1,
+  name: req.body.name,
+  domain: req.body.domain,
+};
+greekGods.push(newGreekGod);
+res.status(201).send({data: newGreekGod});
+
+});
+
+//API to update a greek god
+app.put("/greekgods/:id", (req, res) => {
+  const id = Number(req.params.id); //req.params vil alene altid returnere en String, derfor ændres..
+  const index = greekGods.findIndex((god) => god.id === id);
+
+  if(index === -1) { //vi vil gerne håndtere fejlen først
+    res.status(404).send({ errorMessage: `Greek god not found with id: ${id}`});
+  } 
+
+  greekGods[index] = {id, ...req.body};
+  res.status(200).send({ data: greekGods[index]});
+
+});
+
+//API to delete a greek god
+app.delete("/greekgods/:id", (req,res) => {
+  const id = Number(req.params.id); //req.params vil alene altid returnere en String, derfor ændres..
+  const index = greekGods.findIndex((god) => god.id === id);
+
+  if(index === -1) { //vi vil gerne håndtere fejlen først
+    res.status(404).send({ errorMessage: `Greek god not found with id: ${id}`});
+  } 
+
+  const deletedGreekGod = greekGods.splice(index,1);
+  res.status(200).send({ data: deletedGreekGod[0]});
+});
 
 
 app.listen(8080); //8080 er http developer port som standard  denne skal være i bunden ellers kan der opstå nogle bugs
