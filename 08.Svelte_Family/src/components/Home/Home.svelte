1f6ffc527a066822dac96537e4e3a1aaca877088
Mette Munch <meha0005@stud.ek.dk>
2025-10-30 09:46:48 +0100

    Added 08.svelte project

---
+++ 08.Svelte_Family/src/components/Home/Home.svelte
@@ -0,0 +1,16 @@
+
+
+<slot></slot>
+
+<h2 id="welcome-mat">Velkommen til måtten</h2>
+
+<style>
+    #welcome-mat {
+        color:black;
+        background-color: aquamarine;
+        padding: 2em;
+        border-radius: 0 0 2em 2em;
+
+    }
+
+</style>
---
+++ 08.Svelte_Family/src/components/Parent/Parent.svelte
@@ -0,0 +1,11 @@
+<script>
+
+    const { parentName, children } = $props();
+    
+</script>
+
+<h1>{ parentName}</h1>
+
+{#each children as child (child.name)} 
+<h3>{child.name}</h3>
+{/each}
